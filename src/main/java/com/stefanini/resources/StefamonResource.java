e87758b7a3213cc831c3d94720944a20ba1ff982
Nora <norahumasi@gmail.com>
2023-02-26 14:01:46 -0300

    Alterando anotacao de @ApplicationPath para @Path no JogadorResource e StefamonResource

--- src/main/java/com/stefanini/resources/StefamonResource.java
+++ src/main/java/com/stefanini/resources/StefamonResource.java
@@ -3,13 +3,12 @@ package com.stefanini.resources;
 import com.stefanini.service.StefamonService;
 
 import javax.inject.Inject;
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@ApplicationPath("/stefamon")
+@Path("/stefamon")
 public class StefamonResource {
 
     @Inject
